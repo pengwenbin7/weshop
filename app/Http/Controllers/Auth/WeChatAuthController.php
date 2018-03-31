dac366e37eac6d7104c66b509f8b2b89aca0e35e
pengwenbin7 <pengwenbin7@126.com>
2018-03-31 09:51:03 +0800

    debug

--- app/Http/Controllers/Auth/WeChatAuthController.php
+++ app/Http/Controllers/Auth/WeChatAuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\ShopUser;
+use EasyWeChat;
 use Auth;
 
 class WeChatAuthController extends Controller
