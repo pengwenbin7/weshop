8e258aabd290c99fd06a98504294a9def6bbbe0d
pengwenbin7 <pengwenbin7@126.com>
2018-03-31 18:46:10 +0800

    debug

--- resources/views/wechat/index.blade.php
+++ resources/views/wechat/index.blade.php
@@ -1,12 +1,14 @@
 @extends("layouts.wechat")
 
 @section("content")
+  <a href="{{ route("wechat.index") }}">link</a>
   <button id="share">分享到朋友圈</button>
 @endsection
 
 @section("script")
   <script>
   $("#share").click(function () {
+    alert("clicked");
     wx.onMenuShareAppMessage({
       title: '互联网之子',
       desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
