da181cedc49fe289244f2505dad1b211343e7475
pengwenbin7 <pengwenbin7@126.com>
2018-03-31 18:23:38 +0800

    jssdk share test

--- app/Http/Controllers/WeChat/IndexController.php
+++ app/Http/Controllers/WeChat/IndexController.php
@@ -4,16 +4,23 @@ namespace App\Http\Controllers\WeChat;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Auth;
 
 class IndexController extends Controller
 {
     public function index(Request $request)
     {
         $user = session("wechat_user");
-        return [
-            "openid" => $user->id,
-            "nikename" => $user->nickname,
+        
+        $interfaces = [
+            "onMenuShareTimeline", "onMenuShareAppMessage",
+            "onMenuShareQQ", "scanQRCode",
+            "chooseWXPay", "getLocation",
+            "chooseImage", "previewImage",
+            "uploadImage", "downloadImage",
         ];
+        return view("wechat.index", [
+            "user" => $user,
+            "interfaces" => $interfaces,
+        ]);
     }
 }
--- resources/views/wechat/index.blade.php
+++ resources/views/wechat/index.blade.php
@@ -1,5 +1,25 @@
 @extends("layouts.wechat")
 
 @section("content")
-  <code>int a = 3 / 2;</code>
+  <button id="share">分享到朋友圈</button>
+@endsection
+
+@section("script")
+  <script>
+  $(document).ready(function () {
+    $("#share").click(function () {
+      wx.onMenuShareTimeline({
+	title: "分享标题",
+	link: "{{ route("wechat.index") }}",
+	imgUrl: "",
+	success: function () {
+	  alert("Nice, share + 1!");
+	},
+	cancel: function () {
+	  alert("Wooooole, cancel sharing!");
+	}
+      });
+    });
+  });
+  </script>
 @endsection
