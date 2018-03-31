e6b3101a3adea8a01ac5eac24ef790c26c6ce5e0
pengwenbin7 <pengwenbin7@126.com>
2018-03-31 18:29:08 +0800

    share debug

--- resources/views/wechat/index.blade.php
+++ resources/views/wechat/index.blade.php
@@ -6,20 +6,18 @@
 
 @section("script")
   <script>
-  $(document).ready(function () {
-    $("#share").click(function () {
-      wx.onMenuShareTimeline({
-	title: "分享标题",
-	link: "{{ route("wechat.index") }}",
-	imgUrl: "",
-	success: function () {
-	  alert("Nice, share + 1!");
-	},
-	cancel: function () {
-	  alert("Wooooole, cancel sharing!");
-	}
-      });
-    });
+  
+  wx.onMenuShareTimeline({
+    title: "分享标题",
+    link: "{{ route("wechat.index") }}",
+    imgUrl: "",
+    success: function () {
+      alert("Nice, share + 1!");
+    },
+    cancel: function () {
+      alert("Wooooole, cancel sharing!");
+    }
   });
+  
   </script>
 @endsection
