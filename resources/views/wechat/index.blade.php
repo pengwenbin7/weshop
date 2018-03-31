046afeee29176fc205e93e7dd65837ac1dd5eb0d
pengwenbin7 <pengwenbin7@126.com>
2018-03-31 18:50:59 +0800

    scan test

--- resources/views/wechat/index.blade.php
+++ resources/views/wechat/index.blade.php
@@ -1,8 +1,9 @@
 @extends("layouts.wechat")
 
 @section("content")
-  <a href="{{ route("wechat.index") }}">link</a>
-  <button id="share">分享到朋友圈</button>
+  <p><a href="{{ route("wechat.index") }}">link</a></p>
+  <p><button id="share">分享到朋友圈</button></p>
+  <p><button id="scan">scan qr</button></p>
 @endsection
 
 @section("script")
@@ -22,6 +23,17 @@
       },
       cancel: function (res) {
         alert('已取消');
+      }
+    });
+  });
+
+  $("#scan").click(function () {
+    wx.scanQRCode({
+      needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+      scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+      success: function (res) {
+	var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+	alert(result);
       }
     });
   });
