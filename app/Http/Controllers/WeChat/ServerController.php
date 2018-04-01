ba18f1e3e85dc9cf2553b1e7fcfe34c79ca3079e
pengwenbin7 <pengwenbin7@126.com>
2018-04-02 01:10:13 +0800

    debug

--- app/Http/Controllers/WeChat/ServerController.php
+++ app/Http/Controllers/WeChat/ServerController.php
@@ -16,17 +16,18 @@ class ServerController extends Controller
         Log::info('request arrived.');
         $app = EasyWeChat::officialAccount();
         $app->server->push(function($message){
-            switch ($message->MsgType) {
+            switch ($message["MsgType"]) {
             case "text":
                 $items = [
                     new NewsItem([
                         'title'       => "测试标题",
-                        'description' => "图文测试http://weshop.mafkj.com/search/" . $message->Content,
-                        'url'         => "http://weshop.mafkj.com/search/" . $message->Content,
+                        'description' => "图文测试http://weshop.mafkj.com/search/" . $message["Content"],
+                        'url'         => "http://weshop.mafkj.com/search/" . $message["Content"],
                         'image'       => "",
                     ]),
                 ];
                 $news = new News($items);
+		return $news;
                 break;
             default:
                 return json_encode($message, JSON_UNESCAPED_UNICODE);
