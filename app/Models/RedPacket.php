a8c642af3e5ed131153ed9f11ee9224ed9568c76
pengwenbin7 <pengwenbin7@126.com>
2018-04-24 15:59:55 +0800

    created red envelope model

--- app/Models/RedPacket.php
+++ app/Models/RedPacket.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class RedPacket extends Model
 {
-    //
+    protected $fillable = [
+        "mch_billno", "mcn_id", "wxappid",
+        "send_name", "re_openid", "total_amount",
+        "wishing", "client_ip", "act_name",
+        "scene_id", "risk_info", "consume_mch_id",
+    ];
 }
--- database/migrations/2018_04_23_225126_create_red_packets_table.php
+++ database/migrations/2018_04_23_225126_create_red_packets_table.php
@@ -13,7 +13,12 @@ class CreateRedPacketsTable extends Migration
      */
     public function up()
     {
+        /**
+         * reference:
+         * https://pay.weixin.qq.com/wiki/doc/api/tools/cash_coupon.php?chapter=13_4&index=3
+         */
         Schema::create('red_packets', function (Blueprint $table) {
+            // 以下为请求参数
             $table->increments('id');
             $table->string("mch_billno", 28);
             $table->string("mch_id", 32);
@@ -23,7 +28,19 @@ class CreateRedPacketsTable extends Migration
             $table->unsignedInteger("total_amount");
             $table->unsignedInteger("total_num")->default(1);
             $table->string("wishing", 128);
-            
+            $table->string("client_ip", 15)->nullable();
+            $table->string("act_name", 32);
+            $table->string("scene_id", 32)->nullable();
+            $table->string("risk_info", 128)->nullable();
+            $table->string("consume_mch_id", 32)->nullable();
+            // 以下为回调参数
+            $table->string("return_code", 16)->comment("SUCCESS/FAIL")->nullable();
+            $table->string("return_msg", 128)->nullable();
+            $table->string("sign", 32)->nullable();
+            $table->string("result_code", 16)->nullable()->comment("SUCCESS/FAIL");
+            $table->string("err_code", 32)->nullable();
+            $table->string("err_code_des", 128)->nullable();
+            $table->string("send_listid", 32)->nullable();
             $table->timestamps();
         });
     }
