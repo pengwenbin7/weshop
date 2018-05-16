cf59d9c6ab97d38ec449841e0902919ffcf4c2fe
pengwenbin7 <pengwenbin7@126.com>
2018-05-16 09:53:48 +0800

    update models: coupon, product-variable

--- app/Models/ProductVariable.php
+++ app/Models/ProductVariable.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class ProductVariable extends Model
 {
     protected $fillable = ["product_id", "unit_price", "stock"];
+
+    public function product()
+    {
+        return $this->belongsTo("App\Models\Prodcut");
+    }
 }
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -10,7 +10,7 @@ class RouteServiceProvider extends ServiceProvider
     /**
      * This namespace is applied to your controller routes.
      *
-     * In addition, it is set as the URL generator's root namespace.
+n     * In addition, it is set as the URL generator's root namespace.
      *
      * @var string
      */
@@ -77,7 +77,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapAdminRoutes()
     {
         Route::group([
-            'middleware' => ['web', 'admin', 'auth:admin'],
+            'middleware' => ['web', 'admin'],
             'prefix' => 'admin',
             'namespace' => $this->namespace,
         ], function ($router) {
