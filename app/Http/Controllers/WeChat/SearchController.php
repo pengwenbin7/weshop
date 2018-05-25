18bd220e657f07e30697990b238f61e038f5b23a
pengwenbin7 <pengwenbin7@126.com>
2018-05-25 08:23:34 +0800

    rebuild index

--- app/Http/Controllers/WeChat/SearchController.php
+++ app/Http/Controllers/WeChat/SearchController.php
@@ -10,6 +10,11 @@ class SearchController extends Controller
 {
     public function search(Request $request)
     {
+        $key = $request->input("key", '');
+        if (!$key) {
+            return Product::with(["brand", "variable"])->all();
+        }
+        $arr = explode("\W", $key);
         $products = Product::where("name", "like", "%{$request->key}%")->get();
         return view("wechat.search",["products" => $products]);
     }
