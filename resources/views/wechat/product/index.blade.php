9124122ccfd23e5f1ea50898336d64ebfdab94ff
bane <bane120@icloud.com>
2018-05-30 17:42:11 +0800

    product-list

--- resources/views/wechat/product/index.blade.php
+++ resources/views/wechat/product/index.blade.php
@@ -6,8 +6,8 @@
   }
 </style>
 <div class="container clearfix" id="category">
-  <div class="category" id="app">
-    <div class="header" ref="header">
+  <div class="category" id="app" v-cloak>
+    <div class="header" ref="header" id="header">
       <div class="search">
         <a class="search-box" href="{{ route("wechat.search") }}">
           <i class="iconfont icon-sousuo"></i><span>快速搜索</span>
@@ -16,8 +16,8 @@
     </div>
     <div class="cat-nav" ref="left">
       <ul id="cat_nav">
-        <li v-for="(item,index) in items" @click="show(item.id)"  v-bind:class='{on:active==index}' v-cloak>
-        <div :class="index == 0 ? 'item on':'item'">
+        <li v-for="(item,index) in items" @click="show(item.id)" :class="active==item.id ? 'item on':'item'" v-cloak>
+        <div >
           <span class="c-name">@{{ item.name }}</span>
         </div>
         </li>
@@ -25,7 +25,7 @@
       </ul>
     </div>
     <div class="goods-list">
-      <div class="filter clearfix">
+      <div class="filter clearfix" id="fillter">
         <div class="item"><span>智能推荐</span></div>
         <div class="item">
           <span>按价格</span>
@@ -42,7 +42,7 @@
           </span>
         </div>
       </div>
-      <div class="products" ref="right">
+      <div class="products" ref="right"  id = "products">
 
         <div class="product" v-for="product in products">
           <a >
@@ -55,12 +55,15 @@
                 <span class="p-bname">@{{ product.name }}</span>
               </p>
               <p class="pirce">
-
+                  <span class="y" v-if="product.is_ton"><i>￥</i>@{{ Number(product.unit_price) }}/吨</span>
+                  <span class="y" v-if="!product.is_ton"><i>￥</i>@{{ product.unit_price }}/@{{ product.packing_unit }}</span>
               </p>
             </div>
           </a>
         </div>
-
+        <div class="" v-on:click="more" id="more" v-if="more_box">
+          <img src="{{ asset("assets/img/timg.gif") }}" width="25" alt="" style="float:left"> <span>正在加载....</span>
+        </div>
 
       </div>
       </div>
@@ -70,15 +73,18 @@
 @endsection
 @section( "script")
 <script type="text/javascript">
-  new Vue({
+  var app = new Vue({
     el: '#app',
     data: {
       items: {!! $categories !!},
-      active: "0",
-      products:{!! $products !!}
+      active: "",
+      products:{!! $products !!},
+      page:1,
+      total:{{ $pcs->lastPage() }},
+      more_box:true,
     },
     mounted: function() {
-
+      this.active =this.items[0].id;
       //获得主体部分高度
       var _height = document.body.clientHeight
       var fontsize = document.documentElement.clientWidth / 7.5;
@@ -90,23 +96,69 @@
     methods: {
       show: function(id) {
         var id = id;
-        // this.active = index;
+        this.active = id;
         this.getDate(id);
+      },
+      more:function() {
+        var id =1;
+        var _this =this;
+        axios.get("{{ route("wechat.product.index") }}"+"?page=2&&id="+id )
+          .then(function(res) {
+          var arr = _this.products.concat(res.data.products)
+          _this.products=arr;
+        })
       },
       getDate: function(id) {
         var _this = this;
         axios.get("{{ route("wechat.product.index") }}"+"?page=1&&id="+id )
           .then(function(res) {
-          console.log(res.data);
-          _this.products = res.data;
+          _this.total=res.data.pcs.total;
+          if(_this.total==1){
+            _this.more_box = false
+          }else{
+            _this.more_box =true;
+          }
+          _this.page = 1;
+          _this.products = res.data.products;
         })
-      },
+      }
     },
     beforeRouteLeave: function() {
-      console.log(1)
-    }
-  })
 
+    }
+  });
+  var me ={ }
+  var more = document.getElementById("more");
+  var element = document.getElementById("products");
+  var height,_height,offsetHeight,rHeight;
+  rHeight = document.getElementById("header").offsetHeight+document.getElementById("fillter").offsetHeight+20;
+  me.touchmoveCall=function(e){
+    height = element.scrollTop;
+    offsetHeight = element.offsetHeight;
+
+    _height = more.offsetTop-rHeight;
+    if(_height<=height+offsetHeight ){
+      var _this = app;
+      if(_this.total>_this.page){
+        _this.page++;
+        if(_this.page == _this.total){
+          _this.more_box = false;
+        }
+      }else{
+        _this.more_box=false;
+         element.addEventListener("touchmove",me.touchmoveCall)
+        return;
+      }
+      element.removeEventListener('touchmove', me.touchmoveCall);
+      axios.get("{{ route("wechat.product.index") }}"+"?page="+_this.page+"&&id=1")
+        .then(function(res) {
+        var arr = _this.products.concat(res.data.products);
+        _this.products=arr;
+        element.addEventListener("touchmove",me.touchmoveCall)
+      })
+    }
+  }
+  element.addEventListener("touchmove",me.touchmoveCall)
 
 </script>
 @endsection
