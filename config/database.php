7701aa95dac7fab0eb8af15ee98cdf2bed73a9fa
19951219 <1092713459@qq.com>
2018-06-27 14:45:20 +0800

    数据2

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'weshop'),
             'username' => env('root', 'root'),
-            'password' => env('root', '123456'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
