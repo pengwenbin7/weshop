7e9b41df5914493788163a96deea210373ccbe0e
pengwenbin7 <pengwenbin7@126.com>
2018-06-27 18:14:53 +0800

    首页不显示下架产品

--- resources/views/wechat/index.blade.php
+++ resources/views/wechat/index.blade.php
@@ -1,8 +1,8 @@
 @extends( "layouts.wechat")
 @section( "content")
-<div class="container">
-  <div class="index" id="app">
-    <div class="search">
+  <div class="container">
+    <div class="index" id="app">
+      <div class="search">
     	<div class="i-search">
     	  <a  href="{{ route("wechat.search") }}">
     	    <input type="text" name="keyword" id="keyword" value="" placeholder="输入关键词快速查找商品" />
@@ -19,32 +19,34 @@
     	    @endforeach
     	  </div>
     	</div>
-    </div>
-    <div class="products" id="product">
-      <div class="title">
-        <span>热卖</span>
       </div>
-      @foreach($products as $product)
-      <div class="product">
-        <a href="{{ route("wechat.product.show", $product->product->id) }}">
-          <div class="prop">
-            <p class="p-name">
-              <span>{{ $product->product->name }}</span>
-              <span>{{ $product->product->model }}</span>
-            </p>
-            <p class="p-bname">
-              <span>{{ $product->product->brand->name }}</span>
-              <span class="p-stock">库存:{{ $product->stock }}</span>
-            </p>
-            <p class="pirce">
-                <span class="y"><i>￥</i>{{ $product->product->price }}</span>
-                <span class="right">{{ $product->product->address  }}</span>
-            </p>
-          </div>
-        </a>
+      <div class="products" id="product">
+	<div class="title">
+          <span>热卖</span>
+	</div>
+	@foreach($products as $product)
+	  @if ($product->active)
+	    <div class="product">
+              <a href="{{ route("wechat.product.show", $product->product->id) }}">
+		<div class="prop">
+		  <p class="p-name">
+		    <span>{{ $product->product->name }}</span>
+		    <span>{{ $product->product->model }}</span>
+		  </p>
+		  <p class="p-bname">
+		    <span>{{ $product->product->brand->name }}</span>
+		    <span class="p-stock">库存:{{ $product->stock }}</span>
+		  </p>
+		  <p class="pirce">
+                    <span class="y"><i>￥</i>{{ $product->product->price }}</span>
+                    <span class="right">{{ $product->product->address  }}</span>
+		  </p>
+		</div>
+              </a>
+	    </div>
+	  @endif
+	@endforeach
       </div>
-      @endforeach
     </div>
   </div>
-</div>
 @endsection
