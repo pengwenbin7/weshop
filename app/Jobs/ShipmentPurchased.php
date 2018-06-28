dc69ab28ba567155f835e18da9adbebc54700a25
pengwenbin7 <pengwenbin7@126.com>
2018-06-28 09:23:57 +0800

    debug shipment shipped

--- app/Jobs/ShipmentPurchased.php
+++ app/Jobs/ShipmentPurchased.php
@@ -8,6 +8,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use App\Models\Shipment;
+use App\Models\Department;
 
 class ShipmentPurchased implements ShouldQueue
 {
