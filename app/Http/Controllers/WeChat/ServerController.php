d2d970a637a05113a6d219ad4a8b12bbda0fd8d4
19951219 <1092713459@qq.com>
2018-07-02 09:02:18 +0800

    分享

--- app/Http/Controllers/WeChat/ServerController.php
+++ app/Http/Controllers/WeChat/ServerController.php
@@ -44,8 +44,25 @@ class ServerController extends Controller
                     User::subRegister($message);
                     return new Text("感谢关注！");
                     break;
+                //分享
+                case "CLICK":
+                    $url = "http://mp.weixin.qq.com/s?__biz=MzIzODY1MjUyNA==
+                    &mid=100000699&idx=1&sn=aed691ad9bae87df98f30d818d5b947f&
+                    chksm=69375eb85e40d7ae812971ce445dbe6a146ff824322e2e815dee9dd0002d2875b23bda67fc6b#rd";
+                    $items = [
+                        new NewsItem([
+                            'title'       => "邀请好友至“太好买”下单，领取现金红包！",
+                            'description' => "详情进【链接】",
+                            'url'         => $url,
+                            'image'       => ''//asset("assets/img/search.jpg"),
+                        ]),
+                    ];
+                    $news = new News($items);
+                    return $news;
+                    break;
                 }
                 break;
+
             case "image":
                 $msg = "你的用户【{$user->name}】给你发了一张<a href=\"{$message["PicUrl"]}\">图片</a>";
                 $user->admin->sendMessage($msg);
