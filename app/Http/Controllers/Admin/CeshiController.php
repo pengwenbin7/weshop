ebf4c3767b2160bd1495c700a79639d658d76335
19951219 <1092713459@qq.com>
2018-07-09 11:55:56 +0800

    测试8

--- app/Http/Controllers/Admin/CeshiController.php
+++ app/Http/Controllers/Admin/CeshiController.php
@@ -20,27 +20,27 @@ class CeshiController extends Controller
     public function index(Request $request)
     {
 
-        $app = EasyWeChat::officialAccount();
-        //群发消息
-        $tags = $app->user_tag->list();//降价消息推送    109
-        $myuser = $app->user_tag->usersOfTag(109, $nextOpenId = '');
-        if (isset($myuser['data']['openid'])) {
-            foreach ($myuser['data']['openid'] as $item) {
-                $app->template_message->send([
-                    'touser' => $item,
-                    'template_id' => 'HPp3ZBtebtk99VZYOGpLRqU7whRKqTlToI7Rq9bLP0Q',
-                    'url' => '',
-                    'data' => [
-                        'first' => '',
-                        'keyword1' => 2222,
-                        'keyword2' => '',
-                        'keyword3' => 3333,
-                        'keyword4' => '',
-                        'remark' => '',
-                    ],
-                ]);
-            }
-        }
+//        $app = EasyWeChat::officialAccount();
+//        //群发消息
+//        $tags = $app->user_tag->list();//降价消息推送    109
+//        $myuser = $app->user_tag->usersOfTag(109, $nextOpenId = '');
+//        if (isset($myuser['data']['openid'])) {
+//            foreach ($myuser['data']['openid'] as $item) {
+//                $app->template_message->send([
+//                    'touser' => $item,
+//                    'template_id' => 'HPp3ZBtebtk99VZYOGpLRqU7whRKqTlToI7Rq9bLP0Q',
+//                    'url' => '',
+//                    'data' => [
+//                        'first' => '',
+//                        'keyword1' => 2222,
+//                        'keyword2' => '',
+//                        'keyword3' => 3333,
+//                        'keyword4' => '',
+//                        'remark' => '',
+//                    ],
+//                ]);
+//            }
+//        }
 
     }
     //设置与发送模板信息
--- app/Listeners/ProductPriceChangedListener.php
+++ app/Listeners/ProductPriceChangedListener.php
@@ -30,28 +30,29 @@ class ProductPriceChangedListener
      */
     public function handle(ProductPriceChangedEvent $event)
     {
-//        $Variable = ProductVariable::where(['product_id' => $event->product->id])
-//            ->orderBy("id", "desc")
-//            ->first();
-//        $app = EasyWeChat::officialAccount();
-//        //群发消息
-//        $myuser = User::pluck('openid');
-//        $users = $app->user->select($myuser);
-//        Log::error($users);
-        //通过模板消息发送降价信息
-//        $app->template_message->send([
-//            'touser' => 'obOoJwWpxqeAPWmN5UNjQOgZZlJM',
-//            'template_id' => 'HPp3ZBtebtk99VZYOGpLRqU7whRKqTlToI7Rq9bLP0Q',
-//            'url' => route("wechat.product.show",$event->product->id),
-//            'data' => [
-//                'first' => '',
-//                'keyword1' => $event->product->name."--".$event->product->model,
-//                'keyword2' => '',
-//                'keyword3' => $Variable->unit_price,
-//                'keyword4' => $event->product->updated_at,
-//                'remark' => '',
-//            ],
-//        ]);
+        $Variable = ProductVariable::where(['product_id' => $event->product->id])
+            ->orderBy("id", "desc")
+            ->first();
+        $app = EasyWeChat::officialAccount();
+         //查询分组推送用户openid
+        $myuser = $app->user_tag->usersOfTag(109, $nextOpenId = '');
+        if (isset($myuser['data']['openid'])) {
+            foreach ($myuser['data']['openid'] as $item) {
+                $app->template_message->send([
+                    'touser' => $item,
+                    'template_id' => 'HPp3ZBtebtk99VZYOGpLRqU7whRKqTlToI7Rq9bLP0Q',
+                    'url' => route("wechat.product.show",$event->product->id),
+                    'data' => [
+                        'first' => '',
+                        'keyword1' => $event->product->name."--".$event->product->model,
+                        'keyword2' => '',
+                        'keyword3' => $Variable->unit_price,
+                        'keyword4' => $event->product->updated_at,
+                        'remark' => '',
+                    ],
+                ]);
+            }
+        }
     }
 
 }
