dc7f7be6f332d611cb75c786e83ce8de8a69adfc
19951219 <1092713459@qq.com>
2018-07-09 14:06:12 +0800

    测试8

--- app/Http/Controllers/Admin/CeshiController.php
+++ app/Http/Controllers/Admin/CeshiController.php
@@ -25,7 +25,7 @@ class CeshiController extends Controller
 //        echo "<pre>";
 //        print_r($user);
         $span = $user->sendMessage(new Image($user->getShareImg()));
-        print_r($span);
+        echo $span;
 //        $app = EasyWeChat::officialAccount();
 //        //群发消息
 //        $tags = $app->user_tag->list();//降价消息推送    109
