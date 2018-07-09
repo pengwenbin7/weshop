63e43766d532165c14ea476752255eae742f6023
19951219 <1092713459@qq.com>
2018-07-09 11:34:08 +0800

    测试8

--- app/Http/Controllers/Admin/CeshiController.php
+++ app/Http/Controllers/Admin/CeshiController.php
@@ -23,11 +23,11 @@ class CeshiController extends Controller
         $app = EasyWeChat::officialAccount();
         //群发消息
         $tags = $app->user_tag->list();//降价消息推送    109
-        $app->user_tag->usersOfTag(109, $nextOpenId = '');
+        $myuser = $app->user_tag->usersOfTag(109, $nextOpenId = '');
 //        $myuser = User::pluck('openid');
 //        $users = $app->user->select(["obOoJwa_IRu2_WqJ3i0a4gQCBc8I","obOoJwa-Wsr3k4JYjIK2OY0bMFS0"]);
         echo "<pre>";
-        print_r($app);
+        print_r($myuser);
     }
     //设置与发送模板信息
     public function index1(){
